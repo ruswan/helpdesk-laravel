50d25a60a57f3a40d176d1bf83613882c2d81494
Kang Ruswan <sukabumikode@gmail.com>
2023-06-07 14:28:06 +0700

    Filament authentication (#2)
    * add filament-authentication
    
    * Remove fialmet shield

---
+++ config/filament-authentication.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'models' => [
+        'User'       => \App\Models\User::class,
+        'Role'       => \Spatie\Permission\Models\Role::class,
+        'Permission' => \Spatie\Permission\Models\Permission::class,
+    ],
+    'resources'     => [
+        'UserResource'       => \Phpsa\FilamentAuthentication\Resources\UserResource::class,
+        'RoleResource'       => \Phpsa\FilamentAuthentication\Resources\RoleResource::class,
+        'PermissionResource' => \Phpsa\FilamentAuthentication\Resources\PermissionResource::class,
+    ],
+    'pages'         => [
+        'Profile' => \Phpsa\FilamentAuthentication\Pages\Profile::class
+    ],
+    'Widgets'       => [
+        'LatestUsers' => [
+            'enabled' => true,
+            'limit'   => 5,
+            'sort'    => 0,
+            'paginate' => false
+        ],
+    ],
+    'preload_roles' => true,
+    'impersonate'   => [
+        'enabled'  => true,
+        'guard'    => 'web',
+        'redirect' => '/'
+    ]
+];
