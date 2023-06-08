fa3d6338fef262cd648d853a0dae0cd7f67f3e26
ruswan <sukabumikode@gmail.com>
2023-06-08 09:08:05 +0700

    require althinect/filament-spatie-roles-permission

---
+++ config/filament-spatie-roles-permissions.php
@@ -0,0 +1,104 @@
+<?php
+
+return [
+
+    'preload_roles' => false,
+
+    'preload_permissions' => false,
+    
+    'navigation_section_group' => 'filament-spatie-roles-permissions::filament-spatie.section.roles_and_permissions', // Default uses language constant
+    
+    'team_model' => \App\Models\Team::class,
+
+    /*
+     * Set to false to remove from navigation
+     */
+    'should_register_on_navigation' => [
+        'permissions' => true,
+        'roles' => true
+    ],
+
+    'generator' => [
+
+        'guard_names' => [
+            'web',
+            //'api'
+        ],
+
+        'permission_affixes' => [
+
+            /*
+             * Permissions Aligned with Policies.
+             * DO NOT change the keys unless the genericPolicy.stub is published and altered accordingly
+             */
+            'viewAnyPermission' => 'view-any',
+            'viewPermission' => 'view',
+            'createPermission' => 'create',
+            'updatePermission' => 'update',
+            'deletePermission' => 'delete',
+            'restorePermission' => 'restore',
+            'forceDeletePermission' => 'force-delete',
+
+            /*
+             * Additional Resource Permissions
+             */
+            'replicate',
+            'reorder',
+        ],
+
+        /*
+         * returns the "name" for the permission.
+         *
+         * $permission which is an iteration of [permission_affixes] ,
+         * $model The model to which the $permission will be concatenated
+         *
+         * Eg: 'permission_name' => 'return $permissionAffix . ' ' . Str::kebab($modelName),
+         *
+         * Note: If you are changing the "permission_name" , It's recommended to run with --clean to avoid duplications
+         */
+        'permission_name' => 'return $permissionAffix . \' \' . $modelName;',
+
+        /*
+         * Permissions will be generated for the models associated with the respective Filament Resources
+         */
+        'discover_models_through_filament_resources' => false,
+
+        /*
+         * Include directories which consists of models.
+         */
+        'model_directories' => [
+            app_path('Models')
+            //app_path('Domains/Forum')
+        ],
+
+        /*
+         * Define custom_models in snake-case
+         */
+        'custom_models' => [
+            //
+        ],
+
+        /*
+         * Define excluded_models in snake-case
+         */
+        'excluded_models' => [
+            //
+        ],
+
+        'excluded_policy_models' => [
+            \App\Models\User::class
+        ],
+
+        /*
+         * Define any other permission here
+         */
+        'custom_permissions' => [
+            //'view-log'
+        ],
+
+
+        'user_model' => \App\Models\User::class,
+
+        'policies_namespace' => 'App\Policies'
+    ],
+];
