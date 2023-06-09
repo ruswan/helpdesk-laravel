5642830f4d661b09bbc2170e467baed7203723da
ruswan <sukabumikode@gmail.com>
2023-06-09 15:40:08 +0700

    Hapus ticket priority

---
+++ database/seeders/TicketSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Ticket;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TicketSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Ticket::create([
+            'priority_id' => 1,
+            'unit_id'   => 1,
+            'owner_id'  => 1,
+            'problem_category_id' => 1,
+            'title' => 'This is a sample ticket',
+            'description' => 'This is a descriptions',
+            'ticket_statuses_id' => '1',
+        ]);
+    }
+}
