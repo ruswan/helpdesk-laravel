30ddf0ca926dda5ce1f4197172b836dee5436a64
ruswan <sukabumikode@gmail.com>
2023-06-09 15:51:56 +0700

    Tambah relasi problem category ke ticket

---
+++ app/Filament/Resources/ProblemCategoryResource/RelationManagers/TicketsRelationManager.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Filament\Resources\ProblemCategoryResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TicketsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'tickets';
+
+    protected static ?string $recordTitleAttribute = 'title';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('owner.name'),
+                Tables\Columns\TextColumn::make('responsible.name'),
+                Tables\Columns\TextColumn::make('ticket_status.name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                //
+            ])
+            ->actions([
+                //
+            ])
+            ->bulkActions([
+                //
+            ]);
+    }
+}
