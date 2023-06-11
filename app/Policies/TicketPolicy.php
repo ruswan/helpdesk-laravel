b17331f96542e798f33540126115eb96f5d7889a
ruswan <sukabumikode@gmail.com>
2023-06-11 10:52:22 +0700

    Change ticket policy

--- app/Policies/TicketPolicy.php
+++ app/Policies/TicketPolicy.php
@@ -4,6 +4,7 @@ namespace App\Policies;
 
 use Illuminate\Auth\Access\Response;
 use App\Models\Ticket;
+use App\Models\TicketStatus;
 use App\Models\User;
 
 class TicketPolicy
@@ -22,12 +23,12 @@ class TicketPolicy
     public function view(User $user, Ticket $ticket): bool
     {
         // The admin unit can view tickets that are assigned to their specific unit.
-        if($user->hasRole('Admin Unit')){
+        if ($user->hasRole('Admin Unit')) {
             return $user->id == $ticket->owner_id || $ticket->unit_id == $user->unit_id;
         }
 
         // The staff unit can view tickets that have been assigned to them.
-        if($user->hasRole('Staff Unit')){
+        if ($user->hasRole('Staff Unit')) {
             return $user->id == $ticket->owner_id ||  $ticket->responsible_id == $user->id;
         }
 
@@ -56,6 +57,10 @@ class TicketPolicy
      */
     public function delete(User $user, Ticket $ticket): bool
     {
+        if ($ticket->ticket_statuses_id != TicketStatus::OPEN) {
+            return false;
+        }
+
         return $user->id == $ticket->owner_id;
     }
 
