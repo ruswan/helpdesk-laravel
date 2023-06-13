63a6045866613432dd753b986c922a5d4ae00459
Ruswan <sukabumikode@gmail.com>
2023-06-13 10:39:31 +0700

    Repait ticket statuses migration

--- database/migrations/2023_06_06_082020_create_ticket_statuses_table.php
+++ database/migrations/2023_06_06_082020_create_ticket_statuses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class() extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +15,8 @@ return new class extends Migration {
         Schema::create('ticket_statuses', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
