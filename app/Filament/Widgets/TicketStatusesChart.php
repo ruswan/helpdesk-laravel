57ff3f7e1d7cfb8784684427fceab538bece030c
Mr. Ruswan <sukabumikode@gmail.com>
2023-07-11 13:32:24 +0700

    Add/chart (#37)
    * require leandrocfe/filament-apex-charts
    
    * publish filament-apex-charts-views
    
    * Add ticket statuses chart in dashboard
    
    ---------
    
    Co-authored-by: Mr. Ruswan <ruswan11@gmail.com>

---
+++ app/Filament/Widgets/TicketStatusesChart.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\TicketStatus;
+use Leandrocfe\FilamentApexCharts\Widgets\ApexChartWidget;
+
+class TicketStatusesChart extends ApexChartWidget
+{
+    /**
+     * Chart Id
+     *
+     * @var string
+     */
+    protected static string $chartId = 'ticketStatusesChart';
+
+    /**
+     * Widget Title
+     *
+     * @var string|null
+     */
+    protected static ?string $heading = 'Ticket Statuses';
+
+    /**
+     * Chart options (series, labels, types, size, animations...)
+     * https://apexcharts.com/docs/options
+     *
+     * @return array
+     */
+    protected function getOptions(): array
+    {
+        $ticketStatuses = TicketStatus::select('id', 'name')->withCount(['tickets'])->get();
+        return [
+            'chart' => [
+                'type' => 'pie',
+                'height' => 300,
+            ],
+            'series' => $ticketStatuses->pluck('id')->toArray(),
+            'labels' => $ticketStatuses->pluck('name')->toArray(),
+            'legend' => [
+                'labels' => [
+                    'colors' => '#9ca3af',
+                    'fontWeight' => 600,
+                ],
+            ],
+        ];
+    }
+}
